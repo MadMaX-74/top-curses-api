f17d81755734d3727cf4f8aea03800ef4832f37a
MaksimMaltsev <m.maltsev@dsec.com>
2023-03-04 20:58:38 +0300

    add env

--- src/top-page/top-page.controller.ts
+++ src/top-page/top-page.controller.ts
@@ -12,14 +12,18 @@ import { ProductModel } from '../product/product.model';
 import { FindProductDto } from '../product/dto/find-product.dto';
 import { TopPageModel } from './top-page.model';
 import { FindTopPageDto } from './dto/find-topPage.dto';
+import { ConfigService } from "@nestjs/config";
 
 @Controller('top-page')
 export class TopPageController {
+  constructor(private readonly configService: ConfigService) {}
   @Post('create')
   async create(@Body() dto: Omit<TopPageModel, '_id'>) {}
 
-  @Get(':id')
-  async get(@Param('id') id: string) {}
+  @Get('get/:alias')
+  async get(@Param('alias') alias: string): Promise<TopPageModel> {
+    this.configService('TEST');
+  }
 
   @Delete(':id')
   async delete(@Param('id') id: string) {}
