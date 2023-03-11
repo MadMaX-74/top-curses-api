ed22899c1907e1e48948c089e46cd5d32c9da71a
MaksimMaltsev <m.maltsev@dsec.com>
2023-03-11 22:32:03 +0300

    add e2e and unit test for review

--- src/review/review.model.ts
+++ src/review/review.model.ts
@@ -1,5 +1,6 @@
 import { Base, TimeStamps } from '@typegoose/typegoose/lib/defaultClasses';
 import { prop } from '@typegoose/typegoose';
+import { Types } from 'mongoose';
 
 export interface ReviewModel extends Base {}
 export class ReviewModel extends TimeStamps{
@@ -10,7 +11,7 @@ export class ReviewModel extends TimeStamps{
   @prop()
   description: string;
   @prop()
-  rating: string;
+  rating: number;
   @prop()
-  createAt: Date;
+  productId: Types.ObjectId;
 }
