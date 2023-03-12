12079cf91ef4c908193996661dcd42a7b57350fd
MaksimMaltsev <m.maltsev@dsec.com>
2023-03-12 21:33:14 +0300

    add auth methods

---
+++ src/auth/auth.constnce.ts
@@ -0,0 +1,1 @@
+export const ALREADY_RIGISTERED_ERROR = 'This user has been register';
--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,10 +1,30 @@
-import { Body, Controller, HttpCode, Post } from '@nestjs/common';
+import {
+  BadRequestException,
+  Body,
+  Controller,
+  HttpCode,
+  HttpException,
+  Post,
+  UsePipes,
+  ValidationPipe
+} from '@nestjs/common';
 import { AuthDto } from './dto/auth.dto';
+import { AuthService } from './auth.service';
+import { ALREADY_RIGISTERED_ERROR } from './auth.constnce';
 
 @Controller('auth')
 export class AuthController {
+  constructor(private readonly authService: AuthService) {
+  }
+  @UsePipes(new ValidationPipe())
   @Post('register')
-  async register(@Body() dto: AuthDto) {}
+  async register(@Body() dto: AuthDto) {
+    const oldUser = await this.authService.findUser(dto.login);
+    if (oldUser) {
+      throw new BadRequestException(ALREADY_RIGISTERED_ERROR);
+    }
+    return this.authService.creatUser(dto)
+  }
   @HttpCode(200)
   @Post('login')
   async login(@Body() dto: AuthDto) {}
